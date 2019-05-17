73232e37423d222ccda4fd9f0abbbfc6e4f08655
Andriy <tranch.manch@gmail.com>
2019-05-17 16:03:32 +0300

    var naming

--- test.js
+++ test.js
@@ -7,21 +7,21 @@
 
 let array = [-7, -1, 6,-10, -8, 5, -5, 4, 3,-2, 9, -2, 1, -9];
 let arrLength = array.length + 1;
-let arrSub = [];
+let arrSum = [];
 let arrTemp = [];
 let sum2 = 0;
 let sumTemp = 0;
 
 
-for (let index1 = 0; index1 < arrLength; index1++) {
-	for (let index2 = 0; index2 < arrLength; index2++) {
-		// console.log(`index1 is ${index1} index2 is ${index2} - slice : ${array.slice(index1, index2)}`);
-		arrSub = array.slice(index1, index2);
-		sumTemp = arrSub.reduce((akk, val) => akk + val, 0);
+for (let indexStart = 0; indexStart < arrLength; indexStart++) {
+	for (let indexEnd = 1; indexEnd < arrLength; indexEnd++) {
+		// console.log(`indexStart is ${indexStart} indexEnd is ${indexEnd} - slice : ${array.slice(indexStart, indexEnd)}`);
+		arrSum = array.slice(indexStart, indexEnd);
+		sumTemp = arrSum.reduce((akk, val) => akk + val, 0);
 		// console.log(sumTemp);
 		if (sumTemp > sum2) {
 			sum2 = sumTemp;
-			arrTemp = arrSub;
+			arrTemp = arrSum;
 		}
 	}
 
