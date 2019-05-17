efdf170af1e787b91782bd3216eff621eb40d4af
Andriy <tranch.manch@gmail.com>
2019-05-17 16:49:51 +0300

    addied accumulator to starts second loop

--- test.js
+++ test.js
@@ -5,26 +5,28 @@
 * */
 
 
-let array = [-7, -1, 6,-10, -8, 5, -5, 4, 3,-2, 9, -2, 1, -9];
+let array = [-7, -1, 6, -10, -8, 5, -5, 4, 3, -2, 9, -2, 1, -9];
 let arrLength = array.length + 1;
 let arrSum = [];
 let arrTemp = [];
 let sum2 = 0;
 let sumTemp = 0;
+let indexEnd = 2;
+let indexStart = 0;
 
-
-for (let indexStart = 0; indexStart < arrLength; indexStart++) {
-	for (let indexEnd = 1; indexEnd < arrLength; indexEnd++) {
+for (indexStart; indexStart < arrLength; indexStart++) {
+	for (indexEnd; indexEnd < arrLength; indexEnd++) {
 		// console.log(`indexStart is ${indexStart} indexEnd is ${indexEnd} - slice : ${array.slice(indexStart, indexEnd)}`);
 		arrSum = array.slice(indexStart, indexEnd);
+		console.log(`slice is ${arrSum}`);
 		sumTemp = arrSum.reduce((akk, val) => akk + val, 0);
-		// console.log(sumTemp);
+		console.log(`summ is is ${sumTemp}`);
 		if (sumTemp > sum2) {
 			sum2 = sumTemp;
 			arrTemp = arrSum;
 		}
 	}
-
+	indexEnd = indexStart + 2;
 }
 console.log(sum2);
 
